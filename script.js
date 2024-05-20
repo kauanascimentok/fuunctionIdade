9895cc35c0d2dd442e67902b65fd887405eb9c5b
kauanascimentok <kaua.nascimento7@aluno.senai.br>
2024-05-20 15:16:58 -0500

    subindo arquivo

---
+++ script.js
@@ -0,0 +1,16 @@
+let idade = prompt("digite sua idade")
+ 
+    if(idade >=18){
+     return alert("voce pode dirigir.");
+   
+    }else if (idade <=18){
+     return alert ("voce não pode dirigir")
+    }else {
+        return alert("idade valida");
+
+    }
+
+    
+    
+    
+ 
